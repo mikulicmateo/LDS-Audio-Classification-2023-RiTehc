9c7206694098742f66f48217b4f8bebc5eb840d7
Mateo Mikulić <mikulic.dev@gmail.com>
2023-03-10 00:33:53 +0100

    Added data augmentation with AudioUtil class to Dataset class

--- data/IRMASDataset.py
+++ data/IRMASDataset.py
@@ -2,13 +2,29 @@ from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
 import os
+import sys
+
+from utils.AudioUtil import AudioUtil
+
+sys.path.insert(0, '../utils/')
 
 
 class IRMASDataset(Dataset):
 
-    def __init__(self, annotations_file, project_dir):
+    def __init__(self, annotations_file, project_dir, new_samplerate, new_channels, new_length, shift_limit, n_mels,
+                 n_fft, mask_percent, n_freq_masks, n_time_masks, hop_len=None):
         self.annotations = pd.read_csv(annotations_file)
         self.project_dir = project_dir
+        self.new_samplerate = new_samplerate
+        self.new_channels = new_channels
+        self.new_length = new_length
+        self.shift_limit = shift_limit
+        self.n_mels = n_mels
+        self.n_fft = n_fft
+        self.hop_len = hop_len
+        self.mask_percent = mask_percent
+        self.n_freq_masks = n_freq_masks
+        self.n_time_masks = n_time_masks
 
     def __len__(self):
         return len(self.annotations)
@@ -16,8 +32,15 @@ class IRMASDataset(Dataset):
     def __getitem__(self, index):
         audio_sample_path = self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
-        signal, sr = torchaudio.load(audio_sample_path)
-        return signal, label
+        audio = torchaudio.load(audio_sample_path)
+        resampled = AudioUtil.resample(audio, self.new_samplerate)
+        rechanneled = AudioUtil.rechannel(resampled, self.new_channels)
+        resized = AudioUtil.pad_trunc(rechanneled, self.new_length)
+        reshifted = AudioUtil.time_shift(resized, self.shift_limit)
+        spectrogram = AudioUtil.generate_spectrogram(reshifted, self.n_mels, self.n_fft, self.hop_len)
+        augmented_spectrogram = AudioUtil.spectrogram_augment(spectrogram, self.mask_percent, self.n_freq_masks,
+                                                              self.n_time_masks)
+        return augmented_spectrogram, label
 
     def _get_audio_sample_path(self, index):
         return os.path.join(self.project_dir, self.annotations.iloc[index, 1])
@@ -30,7 +53,37 @@ class IRMASDataset(Dataset):
 if __name__ == "__main__":
     ANNOTATIONS_FILE = '/home/mateo/Lumen-data-science/LDS-Audio-Classification-2023-RiTehc/IRMAS_Training_Data/training_annotation_file.csv'
     PROJECT_DIR = '/home/mateo/Lumen-data-science/LDS-Audio-Classification-2023-RiTehc'
-    ds = IRMASDataset(ANNOTATIONS_FILE, PROJECT_DIR)
+    NEW_SAMPLERATE = 16000
+    NEW_CHANNELS = 1
+    NEW_LENGTH_MS = 3000
+    SHIFT_PERCENT = 0.1
+    N_MELS = 64
+    N_FFT = 1024
+    MAX_MASK_PERCENT = 0.1
+    N_FREQ_MASKS = 2
+    N_TIME_MASKS = 2
+    HOP_LEN = None
+
+    ds = IRMASDataset(
+        ANNOTATIONS_FILE,
+        PROJECT_DIR,
+        NEW_SAMPLERATE,
+        NEW_CHANNELS,
+        NEW_LENGTH_MS,
+        SHIFT_PERCENT,
+        N_MELS,
+        N_FFT,
+        MAX_MASK_PERCENT,
+        N_FREQ_MASKS,
+        N_TIME_MASKS,
+        HOP_LEN
+    )
 
     print(f'There are {len(ds)} samples')
     # signal, label = ds[0]
+
+    # import matplotlib.pyplot as plt
+
+    # plt.imshow(signal[0])
+    # plt.show()
+
---
+++ utils/AudioUtil.py
@@ -0,0 +1,93 @@
+import torch
+import torchaudio
+import random
+from torchaudio import transforms
+
+
+class AudioUtil:
+
+    @staticmethod
+    def rechannel(audio, new_channel):
+        signal, sr = audio
+
+        if signal.shape[0] == new_channel:
+            return audio
+
+        if new_channel == 1:
+            resignal = torch.mean(signal, dim=0, keepdim=True)  # signal[:1, :]
+        else:
+            resignal = torch.cat([signal, signal])
+
+        return resignal, sr
+
+    @staticmethod
+    def resample(audio, newsr):
+        signal, sr = audio
+
+        if sr == newsr:
+            return audio
+
+        num_channels = signal.shape[0]
+        resignal = torchaudio.transforms.Resample(sr, newsr)(signal[:1, :])
+
+        if num_channels > 1:
+            retwo = torchaudio.transforms.Resample(sr, newsr)(signal[1:, :])
+            resignal = torch.cat([resignal, retwo])
+
+        return resignal, newsr
+
+    @staticmethod
+    def pad_trunc(audio, max_ms):
+        signal, sr = audio
+        num_rows, signal_len = signal.shape
+
+        max_len = sr // 1000 * max_ms
+        if signal_len > max_len:
+            signal = signal[:, :max_len]
+
+        elif signal_len < max_len:
+            # Length of padding to add at the beginning and end of the signal
+            pad_begin_len = random.randint(0, max_len - signal_len)
+            pad_end_len = max_len - signal_len - pad_begin_len
+            # Pad with 0s
+            pad_begin = torch.zeros((num_rows, pad_begin_len))
+            pad_end = torch.zeros((num_rows, pad_end_len))
+            signal = torch.cat((pad_begin, signal, pad_end), 1)
+
+        return signal, sr
+
+    @staticmethod
+    def time_shift(audio, shift_limit):
+        signal, sr = audio
+        _, signal_len = signal.shape
+        shift_amt = int(random.random() * shift_limit * signal_len)
+        return signal.roll(shift_amt), sr
+
+    @staticmethod
+    def generate_spectrogram(audio, n_mels=64, n_fft=1024, hop_len=None):
+        signal, sr = audio
+        top_db = 80
+
+        make_spectrogram = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)
+        spec = make_spectrogram(signal)
+
+        convert_to_db = transforms.AmplitudeToDB(top_db=top_db)
+        spec = convert_to_db(spec)
+
+        return spec
+
+    @staticmethod
+    def spectrogram_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+        _, n_mels, n_steps = spec.shape
+        mask_value = spec.mean()
+        aug_spec = spec
+        freq_mask_param = max_mask_pct * n_mels
+
+        for _ in range(n_freq_masks):
+            aug_spec = transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+
+        time_mask_param = max_mask_pct * n_steps
+        for _ in range(n_time_masks):
+            aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+
+        return aug_spec
