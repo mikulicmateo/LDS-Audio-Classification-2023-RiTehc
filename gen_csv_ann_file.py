77da8a73ae25714c46eebb6efab29df9653926af
VicevicDominik <vicevic.dominik@gmail.com>
2023-03-12 13:09:59 +0100

    Multiclass conversion for training csv file

--- gen_csv_ann_file.py
+++ gen_csv_ann_file.py
@@ -1,14 +1,47 @@
 import os
 import pandas as pd
 
-os.chdir(r'IRMAS_Training_Data')
+current_dir = os.getcwd()
+training_dataset_path = r'.../IRMAS_Training_Data'
+os.chdir(training_dataset_path)
 
 folders = ['tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
+columns = ['path', 'tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
+type = {
+    "tru": 0,
+    "gac": 0,
+    "sax": 0,
+    "cel": 0,
+    "flu": 0,
+    "gel": 0,
+    "vio": 0,
+    "cla": 0,
+    "pia": 0,
+    "org": 0,
+    "voi": 0
+}
+
 
 files = []
 
 for folder in folders:
-    for file in os.listdir(folder):
-        files.append([os.path.join('IRMAS_Training_Data', folder, file), folder, folders.index(folder)])
+    type.update({str(folder): 1})
 
-pd.DataFrame(files, columns=['path', 'class', 'classId']).to_csv('training_annotation_file.csv')
+    for file in os.listdir(folder):
+        files.append([os.path.join('IRMAS_Training_Data', folder, file), 
+                      type.get("tru"), 
+                      type.get("gac"), 
+                      type.get("sax"), 
+                      type.get("cel"), 
+                      type.get("flu"), 
+                      type.get("gel"), 
+                      type.get("vio"),
+                      type.get("cla"), 
+                      type.get("pia"), 
+                      type.get("org"), 
+                      type.get("voi")])
+        
+    type.update({str(folder): 0})
+
+os.chdir(os.path.join(current_dir, 'IRMAS_Training_Data'))
+pd.DataFrame(files, columns=columns).to_csv('training_annotation_file.csv')
