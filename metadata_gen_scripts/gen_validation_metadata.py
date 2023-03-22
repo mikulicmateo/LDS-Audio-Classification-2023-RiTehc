edc3a610319ae6c5eeee0c4451c4093492d6db70
VicevicDominik <vicevic.dominik@gmail.com>
2023-03-22 01:16:36 +0100

    Generation of plots and distributions for instrument mix ratios and specific number of instruments

--- metadata_gen_scripts/gen_validation_metadata.py
+++ metadata_gen_scripts/gen_validation_metadata.py
@@ -3,13 +3,12 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-working_dir = os.path.dirname(os.getcwd())
-validation_dataset_path = r'../IRMAS_Validation_Data/'
+working_dir = os.getcwd()
+validation_dataset_path = r'/home/dominik/Work/Lumen Datascience/Dataset/IRMAS_Validation_Data/'
 os.chdir(validation_dataset_path)
 
-columns = ['type', 'tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
-data_raw = []
-data_percentages = []
+columns = ['type', 'num_of_instruments', 'tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
+columns_instrument_num = ['type', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 all_instruments_counter = {
     "tru": 0,
@@ -23,20 +22,6 @@ all_instruments_counter = {
     "pia": 0,
     "org": 0,
     "voi": 0
-} 
-
-specific_instrument_counter = {
-    "tru": all_instruments_counter.copy(),
-    "gac": all_instruments_counter.copy(),
-    "sax": all_instruments_counter.copy(),
-    "cel": all_instruments_counter.copy(),
-    "flu": all_instruments_counter.copy(),
-    "gel": all_instruments_counter.copy(),
-    "vio": all_instruments_counter.copy(),
-    "cla": all_instruments_counter.copy(),
-    "pia": all_instruments_counter.copy(),
-    "org": all_instruments_counter.copy(),
-    "voi": all_instruments_counter.copy()
 }
 
 instrument_mix_counter = {
@@ -53,99 +38,49 @@ instrument_mix_counter = {
     "voi": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 }
 
-solo_instrument_counter = {
-    "tru": 0,
-    "gac": 0,
-    "sax": 0,
-    "cel": 0,
-    "flu": 0,
-    "gel": 0,
-    "vio": 0,
-    "cla": 0,
-    "pia": 0,
-    "org": 0,
-    "voi": 0
-}
-
-two_instrument_counter = {
-    "tru": all_instruments_counter.copy(),
-    "gac": all_instruments_counter.copy(),
-    "sax": all_instruments_counter.copy(),
-    "cel": all_instruments_counter.copy(),
-    "flu": all_instruments_counter.copy(),
-    "gel": all_instruments_counter.copy(),
-    "vio": all_instruments_counter.copy(),
-    "cla": all_instruments_counter.copy(),
-    "pia": all_instruments_counter.copy(),
-    "org": all_instruments_counter.copy(),
-    "voi": all_instruments_counter.copy()
-}
-
-three_instrument_counter = {
-    "tru": all_instruments_counter.copy(),
-    "gac": all_instruments_counter.copy(),
-    "sax": all_instruments_counter.copy(),
-    "cel": all_instruments_counter.copy(),
-    "flu": all_instruments_counter.copy(),
-    "gel": all_instruments_counter.copy(),
-    "vio": all_instruments_counter.copy(),
-    "cla": all_instruments_counter.copy(),
-    "pia": all_instruments_counter.copy(),
-    "org": all_instruments_counter.copy(),
-    "voi": all_instruments_counter.copy()
+specific_instrument_counter = {
+    "tru": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "gac": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "sax": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "cel": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "flu": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "gel": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "vio": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "cla": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "pia": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "org": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()],
+    "voi": [0, all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy(), all_instruments_counter.copy()]
 }
 
 
-four_instrument_counter = {
-    "tru": all_instruments_counter.copy(),
-    "gac": all_instruments_counter.copy(),
-    "sax": all_instruments_counter.copy(),
-    "cel": all_instruments_counter.copy(),
-    "flu": all_instruments_counter.copy(),
-    "gel": all_instruments_counter.copy(),
-    "vio": all_instruments_counter.copy(),
-    "cla": all_instruments_counter.copy(),
-    "pia": all_instruments_counter.copy(),
-    "org": all_instruments_counter.copy(),
-    "voi": all_instruments_counter.copy()
-}
 
-five_instrument_counter = {
-    "tru": all_instruments_counter.copy(),
-    "gac": all_instruments_counter.copy(),
-    "sax": all_instruments_counter.copy(),
-    "cel": all_instruments_counter.copy(),
-    "flu": all_instruments_counter.copy(),
-    "gel": all_instruments_counter.copy(),
-    "vio": all_instruments_counter.copy(),
-    "cla": all_instruments_counter.copy(),
-    "pia": all_instruments_counter.copy(),
-    "org": all_instruments_counter.copy(),
-    "voi": all_instruments_counter.copy()
-}
-
-def create_plot(dictionary, instrument_name):
+def create_distribution_plot(dictionary, instrument_name, data_raw, data_percentages, num_of_instruments = 0):
     sum = np.sum(list(dictionary.values()))
-    temp_raw = [instrument_name]
-    temp_percentage = [instrument_name]
+    temp_raw = [instrument_name, num_of_instruments]
+    temp_percentage = [instrument_name, num_of_instruments]
 
     for key in dictionary.keys():
         temp_raw.append(dictionary[key])
-        dictionary[key] = dictionary[key] / sum
+        if dictionary[key] != 0:
+            dictionary[key] = dictionary[key] / sum
         temp_percentage.append(dictionary[key])
 
     plt.bar(dictionary.keys(), dictionary.values(), width = 0.9)
-    plt.savefig('validation_' + instrument_name + '_instruments_count.png')
+    if num_of_instruments != 0:
+        plt.savefig('validation_' + instrument_name + '_' + str(num_of_instruments) + '_instruments_count.png')
+    else:
+        plt.savefig('validation_' + instrument_name + '_instruments_count.png')
     plt.clf()
 
     data_raw.append(temp_raw)
     data_percentages.append(temp_percentage)
 
+    return (data_raw, data_percentages)
+
+
 
 files = os.listdir(os.getcwd())
 files.sort()
-
-array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for i in range(0, len(files)-1, 2):
     txt_file = files[i]
@@ -161,7 +96,6 @@ for i in range(0, len(files)-1, 2):
         instruments = f.readlines()
         num_of_instruments = len(instruments)
 
-        array[num_of_instruments - 1] += 1
         for instrument in instruments:
             instrument_name = instrument.strip()
 
@@ -171,38 +105,77 @@ for i in range(0, len(files)-1, 2):
             for companion_instrument in instruments:
                 companion_instrument_name = companion_instrument.strip()
                 if len(instruments) == 1:
-                    solo_instrument_counter[instrument_name] += 1
-                    specific_instrument_counter[instrument_name][companion_instrument_name] += 1
+                    specific_instrument_counter[instrument_name][num_of_instruments - 1] +=1
                 else:
                     if instrument == companion_instrument:
                         continue
+                    specific_instrument_counter[instrument_name][num_of_instruments - 1][companion_instrument_name] += 1
 
-                    if num_of_instruments == 2:
-                        two_instrument_counter[instrument_name][companion_instrument_name] += 1
-                    elif num_of_instruments == 3:
-                        three_instrument_counter[instrument_name][companion_instrument_name] += 1
-                    elif num_of_instruments == 4:
-                        four_instrument_counter[instrument_name][companion_instrument_name] += 1
-                    elif num_of_instruments == 5:
-                        five_instrument_counter[instrument_name][companion_instrument_name] += 1
 
-                    specific_instrument_counter[instrument_name][companion_instrument_name] += 1
-    
-# print(four_instrument_counter['tru']/)
-print(instrument_mix_counter['tru']/np.sum(instrument_mix_counter['tru']))
 
 os.chdir(os.path.join(working_dir, 'IRMAS_Validation_Data'))
 if not os.path.exists(os.path.join(working_dir, 'IRMAS_Validation_Data/Plots')):
     os.makedirs('Plots')
 os.chdir(os.path.join(working_dir, 'IRMAS_Validation_Data/Plots'))
 
-create_plot(all_instruments_counter, 'all')
 
-for key in specific_instrument_counter.keys():
-    create_plot(specific_instrument_counter[key], key)
 
+#Creating instrument mix ratios metadata csv file
+data_raw = []
+data_percentages = []
+
+for instrument in instrument_mix_counter.keys():
+    sum = np.sum(instrument_mix_counter[instrument])
+    temp_raw = [instrument]
+    temp_percentages = [instrument]
+
+    for i in range(len(instrument_mix_counter[instrument])):
+        temp_raw.append(instrument_mix_counter[instrument][i])
+        temp_percentages.append(instrument_mix_counter[instrument][i] / sum)
+    
+    data_raw.append(temp_raw)
+    data_percentages.append(temp_percentages)
+
+pd.DataFrame(data_raw, columns = columns_instrument_num).to_csv('validation_instrument_mix_ratios_metadata_raw.csv')
+pd.DataFrame(data_percentages, columns = columns_instrument_num).to_csv('validation_instrument_mix_ratios_metadata_percentage.csv')
+
+
+
+#Creating all instrument metadata csv file
+data_raw = []
+data_percentages = []
+
+data_raw, data_percentages = create_distribution_plot(all_instruments_counter, 'all', data_raw, data_percentages)
 
 pd.DataFrame(data_raw, columns = columns).to_csv('validation_metadata_raw.csv')
 pd.DataFrame(data_percentages, columns = columns).to_csv('validation_metadata_percentage.csv')
+
+
+
+#Creating metadata csv file for specific instruments
+for instrument in specific_instrument_counter.keys():
+
+    os.chdir(os.path.join(working_dir, 'IRMAS_Validation_Data/Plots'))
+    if not os.path.exists(os.path.join(working_dir, 'IRMAS_Validation_Data/Plots/' + instrument)):
+        os.makedirs(instrument)
+    os.chdir(os.path.join(working_dir, 'IRMAS_Validation_Data/Plots/' + instrument))
+
+    data_raw = []
+    data_percentages = []
+
+    for i in range(len(specific_instrument_counter[instrument])):
+        if i == 0:
+            continue
+
+        data_raw, data_percentages = create_distribution_plot(specific_instrument_counter[instrument][i],
+                                                              instrument,
+                                                              data_raw,
+                                                              data_percentages,
+                                                              i+1)
+
+    pd.DataFrame(data_raw, columns = columns).to_csv(f'validation_{instrument}_metadata_raw.csv')
+    pd.DataFrame(data_percentages, columns = columns).to_csv(f'validation_{instrument}_metadata_percentage.csv')
+
+
 
 print(f'Saved to: {os.getcwd()}')
