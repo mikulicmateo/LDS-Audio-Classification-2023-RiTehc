af076a5ac05c4cbbe568ab424c88b34843ede489
VicevicDominik <vicevic.dominik@gmail.com>
2023-04-30 16:03:41 +0200

    Added uniform generation of genre matching mixed dataset

--- metadata_gen_scripts/gen_csv_training_file.py
+++ metadata_gen_scripts/gen_csv_training_file.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd
 
 current_dir = os.getcwd()
-training_dataset_path = r'IRMAS_Training_Data'
+training_dataset_path = r'/home/dominik/Work/Lumen Datascience/LDS-Audio-Classification-2023-RiTehc/IRMAS_Training_Data'
 os.chdir(training_dataset_path)
 
 folders = ['tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
-columns = ['path', 'tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi']
+columns = ['path', 'tru', 'gac', 'sax', 'cel', 'flu', 'gel', 'vio', 'cla', 'pia', 'org', 'voi', 'genre']
+genres = ['cla', 'jaz_blu', 'pop_roc', 'cou_fol', 'lat_sou']
+
 type = {
     "tru": 0,
     "gac": 0,
@@ -28,6 +30,13 @@ for folder in folders:
     type.update({str(folder): 1})
 
     for file in os.listdir(folder):
+        file_genre = ''
+        for genre in genres:
+            if genre in file:
+                file_genre = genre
+
+        assert file_genre != ''
+
         files.append([os.path.join('IRMAS_Training_Data', folder, file), 
                       type.get("tru"), 
                       type.get("gac"), 
@@ -39,9 +48,10 @@ for folder in folders:
                       type.get("cla"), 
                       type.get("pia"), 
                       type.get("org"), 
-                      type.get("voi")])
-        
+                      type.get("voi"),
+                      file_genre])
+
     type.update({str(folder): 0})
 
-os.chdir(os.path.join(current_dir, 'IRMAS_Training_Data'))
-pd.DataFrame(files, columns=columns).to_csv('training_annotation_file.csv')
+#os.chdir(os.path.join(current_dir, 'IRMAS_Training_Data'))
+pd.DataFrame(files, columns=columns).to_csv('training_annotation_file_with_genres.csv')
